8b174c6e0de77f6a29ac80903c6533f590340cbc
Ketsia Lumiere Donfack Ouwe <ketsiad.udel.edu>
2025-03-16 23:26:54 -0400

    mcq done

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -7,9 +7,25 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [choice, setChoice] = useState<string>(options[0]);
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+
+            <select
+                value={choice}
+                onChange={(e) => {
+                    setChoice(e.target.value);
+                }}
+            >
+                {options.map((option) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+            <span> {choice === expectedAnswer ? "✔️" : "❌"} </span>
         </div>
     );
 }
